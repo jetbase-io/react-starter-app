602943aa6b23d2324c61c625dd54a60e80dd0789
IsabekovTemirlan <temirlanisabekov@MacBook-Pro-Temirlan.local>
2023-02-10 00:45:29 +0600

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import "react-toastify/dist/ReactToastify.min.css";
 
 import { Elements } from "@stripe/react-stripe-js";
--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,11 +1,26 @@
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 
 const NotFoundPage: FC = () => (
-  <div className="min-h-screen bg-gray-50 flex flex-col justify-center">
-    <div className="max-w-md w-full mx-auto">
-      <div className="text-center font-medium text-5xl">404 Page Not Found!</div>
-    </div>
-  </div>
-);
 
+  <main className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+    <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
+    <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+      Page Not Found
+    </div>
+    <button className="mt-5">
+      <a
+        className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
+      >
+        <span
+          className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"
+        ></span>
+
+        <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+          <Link to="/">Go Home</Link>
+        </span>
+      </a>
+    </button>
+  </main>
+);
 export default NotFoundPage;
